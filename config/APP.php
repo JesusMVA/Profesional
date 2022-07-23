658b2f590d4c69dd4c28ad782e712ea63ef3e33e
Sistemas <Sistemas@PALDFPCBIFSP244.FormacionSena.RED>
2022-07-23 11:54:51 -0500

    Primeros cambios

---
+++ config/APP.php
@@ -0,0 +1,11 @@
+<?php 
+
+const SERVERURL="http://localhost:8080/Profesional/";
+
+const COMPANY="JESUS";
+
+const MONEDA="$";
+
+date_default_timezone_set("America/Bogota")
+
+?>
---
+++ config/SERVER.php
@@ -0,0 +1,15 @@
+<?php
+
+const SERVER="localhost";
+const DB="proyecto";
+const USER="root";
+const PASS="";
+
+//SISTEMA GESTOR DE BASE DE DATOS
+const SGBD="mysql:host=".SERVER.";dbname=".DB;
+
+const METHOD="AES-256-CBC";
+const SECRET_KEY='$proyecto@2022';
+
+
+?>
